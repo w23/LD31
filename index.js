9bcad9cf74e8ea48e9ad39527aad002483f652de
Ivan 'w23' Avdeev <me@w23.ru>
2014-12-07 20:08:22 +0600

    oh noes refactoring

--- index.js
+++ index.js
@@ -1,227 +1,368 @@
-var EXP = {}
+var EXP = EXP || {}
 
-var FleetEngine = {
-	nodes: [],
-	fleets: [],
-	players: []
-};
+EXP.FleetEngine = function (slowness, listener) {
+	this.listener = listener;
+	this.fleet_slowness = slowness;
+	this.nodes = [];
+	this.fleets = [];
+	this.players = [];
 
-FleetEngine.Player = function (color) {
-	this.material = new THREE.MeshBasicMaterial({color: color});
-	this.visible_nodes = {};
-	FleetEngine.players.push(this);
-}
-
-var no_player = new FleetEngine.Player(0x808080);
-var player = new FleetEngine.Player(0x00ff00);
-
-FleetEngine.travel_time = function(src, dst) {
-	var dx = src.x - dst.x, dy = src.y - dst.y, dz = src.z - dst.z;
-	return +Math.ceil(Math.sqrt(dx*dx + dy*dy + dz*dz)) * 10;
-}
-
-FleetEngine.pop_fraction = function (node, fract) {
-	var fract = Math.max(0, Math.min(1, fract));
-	return node.population * fract * fract;
-}
-
-FleetEngine.add_fleet = function(player, src, dst, fraction) {
-	var count = this.pop_fraction(src, fraction);
-	if (src.player != player || src.population < count) {
-		return false;
-	}
-	var fleet = {
-		player: player,
-		src: src,
-		dst: dst,
-		count: count,
-		time_left: FleetEngine.travel_time(src, dst)
-	};
-	this.fleets.push(fleet);
-	console.log("fleet", fleet);
-	return true;
-}
-
-FleetEngine.tick = function () {
-	for (var i = 0; i < this.nodes.length; ++i) {
-		var node = this.nodes[i];
-		node.population += node.production * ((node.player == no_player) ? 0.1 : 1);
-		node.player.visible_nodes[node.name].population = node.population;
+	this.createPlayer = function (attrs) {
+		var player = {
+			//this.material = new THREE.MeshBasicMaterial({color: color});
+			growth: +attrs.growth,
+			color: +attrs.color,
+			knowledge: {
+				nodes: {}
+			},
+			fleets: []
+		};
+		/* TODO existing knowledge */
+		this.players.push(player);
+		return player;
 	}
 
-	var arrived = [];
-	var in_flight = [];
-	for (var i = 0; i < this.fleets.length; ++i) {
-		var fleet = this.fleets[i];
-		fleet.time_left -= 1;
-		if (fleet.time_left < 1) {
-			arrived.push(fleet);
-		} else {
-			in_flight.push(fleet);
-		}
-	}
-
-	this.fleets = in_flight;
-
-	for (var i = 0; i < arrived.length; ++i) {
-		var fleet = arrived[i];
-		console.log("arrived", fleet, fleet.dst);
-		var deltapop = 0;
-		if (fleet.player == fleet.dst.player) {
-			deltapop = fleet.count;
-		} else {
-			var attacker = fleet.count * fleet.src.attack;
-			var defender = fleet.dst.population * fleet.dst.defence;
-			deltapop = defender - attacker;
-			console.log(attacker, defender, deltapop);
-			if (deltapop < 0) {
-				deltapop = - Math.floor(deltapop / fleet.src.attack) + 1;
-				fleet.dst.player = fleet.player;
+	this.createNode = function (attr) {
+		var node = {
+			name: attr.name,
+			pos: {
+				x: +attr.pos.x,
+				y: +attr.pos.y,
+				z: +attr.pos.z
+			},
+			player: attr.player,
+			population: 0,
+			production: +attr.production,
+			attack: +attr.attack,
+			defence: +attr.defence,
+			toString: function () { return node.name; }
+		};
+		for (var i = 0; i < this.players.length; ++i) {
+			var player = this.players[i];
+			if (player == node.player) {
+				player.knowledge.nodes[node] = node;
 			} else {
-				deltapop = 1 + Math.floor(deltapop / fleet.dst.defence) - fleet.dst.population;
+				player.knowledge.nodes[node] = {
+					name: node.name,
+					pos: node.pos,
+					player: undefined,
+					population: undefined,
+					production: undefined,
+					attack: undefined,
+					defence: undefined
+				};
 			}
 		}
-		fleet.dst.population += deltapop;
-		player.visible_nodes[fleet.dst.name].population = fleet.dst.population;
+		this.nodes.push(node);
+		return node;
 	}
-}
 
-function generate() {
-	var count = 100;
+	this.travel_time = function(src, dst) {
+		var dx = src.pos.x - dst.pos.x, dy = src.pos.y - dst.pos.y, dz = src.pos.z - dst.pos.z;
+		return +Math.ceil(Math.sqrt(dx*dx + dy*dy + dz*dz)) * this.fleet_slowness;
+	}
+
+	this.sendFleet = function(player, src, dst, count) {
+		//var count = this.pop_fraction(src, fraction);
+		if (src.player != player || src.population < count) {
+			return null;
+		}
+		var total_time = FleetEngine.travel_time(src, dst);
+		var fleet = {
+			player: player,
+			src: src,
+			dst: dst,
+			count: +count,
+			total_time: +total_time,
+			time_left: +total_time
+		};
+		src.population -= count;
+		this.fleets.push(fleet);
+		return fleet;
+	}
+
+	this.tick = function () {
+		// grow population
+		for (var i = 0; i < this.nodes.length; ++i) {
+			var node = this.nodes[i];
+			node.population += node.production * node.player.growth;
+		}
+
+		// process fleets in flight
+		var arrived = [];
+		var in_flight = [];
+		for (var i = 0; i < this.fleets.length; ++i) {
+			var fleet = this.fleets[i];
+			fleet.time_left -= 1;
+			if (fleet.time_left < 1) {
+				arrived.push(fleet);
+			} else {
+				in_flight.push(fleet);
+			}
+		}
+		this.fleets = in_flight;
+
+		// process arrived fleets (TODO deterministic order)
+		for (var i = 0; i < arrived.length; ++i) {
+			var fleet = arrived[i];
+			this.listener.fevFleetArrived(fleet);
+
+			if (fleet.player == fleet.dst.player) {
+				fleet.dst.population += fleet.count;
+			} else {
+				var winner = undefined, loser = undefined;
+				var attacker = fleet.count * fleet.src.attack;
+				var defender = fleet.dst.population * fleet.dst.defence;
+				// capture if attacking force is greater
+				if (attacker > defender) {
+					winner = fleet.player;
+					loser = fleet.dst.player;
+					fleet.dst.population = (attacker - defender) / fleet.src.attack;
+					fleet.dst.player = fleet.player;
+				} else {
+					winner = fleet.player;
+					loser = fleet.dst.player;
+					fleet.dst.population = (defender - attacker) / fleet.dst.defence;
+				}
+
+				// update knowledge
+				winner.knowledge.nodes[fleet.dst] = fleet.dst;
+				loser.knowledge.nodes[fleet.dst] = {
+					name: node.name,
+					pos: node.pos,
+					player: winner,
+					population: fleet.dst.population,
+					production: fleet.dst.production,
+					attack: fleet.dst.attack,
+					defence: fleet.dst.attack
+				}
+
+				// notify listener
+				if (winner == fleet.player) {
+					this.listener.fevNodeCaptured(fleet.dst);
+				}
+			} // if battle
+		} // for all arrived fleets
+	} // function tick()
+} // EXP.FleetEngine
+
+var highlighted;
+
+Stage = function (prevStage) {
+	this._pickable = [];
+	this._scene = new THREE.Scene();
+	this._engine = new EXP.FleetEngine(10, this);
+	this._highlight = null;
+
+	this._no_player = this._engine.createPlayer({growth:0.1, color:0x111111});
+	this._player = this._engine.createPlayer({growth:1, color:0x00ff00});
+
+	this._ai = [
+		this._engine.createPlayer({growth:1, color:0xff00ff*Math.random()}),
+		this._engine.createPlayer({growth:1, color:0xff00ff*Math.random()}),
+		this._engine.createPlayer({growth:1, color:0xff00ff*Math.random()}),
+		this._engine.createPlayer({growth:1, color:0xff00ff*Math.random()})
+	];
+
+	this._ticks_per_second = 20;
+	this._next_tick_delta = 1000.0 / this._ticks_per_second;
+	this._next_tick = Date.now() + this._next_tick_delta;
+
+	this.pop_fraction = function (node, fract) {
+		var fract = Math.max(0, Math.min(1, fract));
+		return node.population * fract * fract;
+	}
+
+	this.makeVisualNode = function (node) {
+		var geom = new THREE.SphereGeometry(node.production * .25);
+		var mat = new THREE.MeshPhongMaterial({
+			ambient: 0x030303,
+			emissive: 0x101010,
+			color: 0xdddddd,
+			specular: 0x009900,
+			shininess: 30
+		});
+		var vnode = new THREE.Mesh(geom, mat);
+		vnode.game_node = node;
+		vnode.position.x = node.pos.x;
+		vnode.position.y = node.pos.y;
+		vnode.position.z = node.pos.z;
+		node.visual_node = vnode;
+		this._scene.add(vnode);
+		this._pickable.push(vnode);
+		return vnode;
+	}
+
+	var count = 20;
 	var SX = 20;
 	var SY = 20;
 	var SZ = 0;
 	var pw_min = 1, pw_max = 4;
 	for (var i = 0; i < count; ++i) {
 		var power = Math.random() * (pw_max - pw_min) + pw_min;
-		var node = {
+		var node = this._engine.createNode({
 			name: 'N' + i.toString(),
-			player: (i == 0) ? player : no_player,
-			x: Math.random() * 2 * SX - SX,
-			y: Math.random() * 2 * SY - SY,
-			z: Math.random() * 2 * SZ - SZ,
-			population: 1,
+			player: (i == 0) ? this._player :
+				(i-1 < this._ai.length) ? this._ai[i-1] : this._no_player,
+			pos: {
+				x: Math.random() * 2 * SX - SX,
+				y: Math.random() * 2 * SY - SY,
+				z: Math.random() * 2 * SZ - SZ,
+			},
 			production: power,
 			attack: 1 / power + Math.random(),
 			defence: 1 + power / 2 + Math.random()
-		};
-		FleetEngine.nodes.push(node);
+		});
+
+		this.makeVisualNode(node);
+	}
+
+	for (var i = 0; i < 6; ++i) {
+		var light = new THREE.PointLight(Math.random() * 0xffffff, 6, 15);
+		light.position.set(
+			Math.random() * 2 * SX - SX,
+			Math.random() * 2 * SY - SY,
+			Math.random() * 2 * SZ - SZ
+		);
+		this._scene.add(light);
+	}
+
+	this.makeVisualFleetLine = function (fleet) {
+		var f = 1.0 - (fleet.time_left / fleet.total_time);
+		lines.vertices.push(
+			new THREE.Vector3(
+				fleet.src.x + (fleet.dst.x-fleet.src.x) * f,
+				fleet.src.y + (fleet.dst.y-fleet.src.y) * f,
+				fleet.src.z + (fleet.dst.z-fleet.src.z) * f
+			),
+			new THREE.Vector3(fleet.dst.x, fleet.dst.y, fleet.dst.z)
+		);
+		var fleetlines = new THREE.Line(lines, player.material, THREE.LinePieces);
+		return line;
+	}
+
+	this.makePreviewLine = function (node, pos) {
+	}
+
+	this.update = function (time) {
+		var ticks_cap = 30;
+		while (time > this._next_tick) {
+			this._engine.tick();
+			this._next_tick += this._next_tick_delta;
+			if (--ticks_cap == 0) {
+				this._next_tick = time + this._next_tick_delta;
+				break;
+			}
+		}
+	}
+
+	this.paint = function (renderer, camera) {
+		/*for (var i = 0; i < FleetEngine.nodes.length; ++i) {
+			var node = FleetEngine.nodes[i];
+			if (node.visual == highlighted) {
+				node.visual.material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+			} else {
+				node.visual.material = node.player.material;
+			}
+		}
+
+		var lines = new THREE.Geometry();
+		for (var i = 0; i < FleetEngine.fleets.length; ++i) {
+			var fleet = FleetEngine.fleets[i];
+		}
+		var fleetlines = new THREE.Line(lines, player.material, THREE.LinePieces);
+
+		scene.add(fleetlines);
+		renderer.render(scene, camera);
+		scene.remove(fleetlines);
+*/
+		/*for (var n in this._player.knowledge.nodes) {
+			var node = this._player.knowledge.nodes[n];
+			if (node.player) {
+				node.visual_node.material.emissive.setHex(node.player.color);
+			}
+		}*/
+
+		for (var i = 0; i < this._engine.nodes.length; ++i) {
+			var node = this._engine.nodes[i];
+			node.visual_node.material.emissive.setHex(node.player.color);
+		}
+
+		if (highlighted) {
+			for (var i = 0; i < highlighted.length; ++i) {
+				var node = highlighted[i];
+				node.emissive_cache = node.visual_node.material.emissive.getHex();
+				node.visual_node.material.emissive.setHex(0xff9999);
+			}
+		}
+
+		renderer.render(this._scene, camera);
+
+		if (highlighted) {
+			for (var i = 0; i < highlighted.length; ++i) {
+				var node = highlighted[i];
+				node.visual_node.material.emissive.setHex(node.emissive_cache);
+			}
+		}
+	}
+
+	this._raycaster = new THREE.Raycaster();
+	this.pick = function (x, y, camera) {
+		var vector = new THREE.Vector3(x, y, camera.near).unproject(camera);
+		this._raycaster.set(camera.position, vector.sub(camera.position ).normalize());
+		var intersects = this._raycaster.intersectObjects(this._pickable);
+
+		if (intersects.length > 0) {
+			return intersects[0].object.game_node;
+		}
+		return null;
 	}
 }
 
 var renderer = null;
 var camera = null;
-var scene = null;
-var raycaster = null;
-
-function generate_visual() {
-	for (var i = 0; i < FleetEngine.nodes.length; ++i) {
-		var node = FleetEngine.nodes[i];
-		//var geom = new THREE.SphereGeometry(0.5);
-		var geom = new THREE.BoxGeometry(1, 1, 1);
-		var mat = node.player.material;
-		var vnode = new THREE.Mesh(geom, mat);
-		vnode.game_node = node;
-		vnode.position.x = node.x;
-		vnode.position.y = node.y;
-		vnode.position.z = node.z;
-		node.visual = vnode;
-		scene.add(vnode);
-
-		for (var j = 0; j < FleetEngine.players.length; ++j) {
-			var player = FleetEngine.players[j];
-			player.visible_nodes[node.name] = {node: node};
-		}
-	}
-}
-
-var raycaster = null;
 var mouse = {x: 0, y: 0};
-var selected = null;
-var next_tick = undefined;
-var ticks_per_second = 20;
-var next_tick_delta = 1000.0 / ticks_per_second;
+var source = null;
+var stage = null;
 
 function main() {
 	renderer = new THREE.WebGLRenderer();
-	renderer.setClearColor(0x0f0f0f, 0);
-	//renderer.autoClear = true;
+	renderer.setClearColor(0x000000, 0);
 
 	var container = document.getElementById('game');
 	container.addEventListener('mousedown', onMouseDown, false);
-	//container.addEventListener('mouseup', onMouseUp, false);
+	container.addEventListener('mouseup', onMouseUp, false);
 	container.addEventListener('mousemove', onMouseMove, false);
 	container.addEventListener('contextmenu', function(e){e.preventDefault(); return false;}, false);
 	window.addEventListener('resize', onWindowResize, false);
 	container.innerHTML = '';
 	container.appendChild(renderer.domElement);
 
-	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1.0, 100.0);
 	camera.position.z = 50;
 	camera.lookAt( new THREE.Vector3(0,0,0) );
-	raycaster = new THREE.Raycaster();
 
-	generate();
-
-	generate_visual();
+	stage = new Stage(null);
 
 	onWindowResize();
 	paint();
-
-	next_tick = Date.now() + next_tick_delta;
 }
 
-function pick () {
-	var x = (mouse.x / window.innerWidth) * 2 - 1;
-	var y = - (mouse.y / window.innerHeight) * 2 + 1;
-
-	var vector = new THREE.Vector3(x, y, camera.near).unproject(camera);
-	raycaster.set(camera.position, vector.sub(camera.position ).normalize());
-	var intersects = raycaster.intersectObjects(scene.children);
-
-	if (intersects.length > 0) {
-		return intersects[0].object;
-	}
-	return null;
+function to_screen(e) {
+	return {
+		x: (e.clientX / window.innerWidth) * 2 - 1,
+		y: - (e.clientY / window.innerHeight) * 2 + 1
+	};
 }
-
-var highlighted = null;
 
 function paint() {
 	requestAnimationFrame(update);
-
-	for (var i = 0; i < FleetEngine.nodes.length; ++i) {
-		var node = FleetEngine.nodes[i];
-		if (node.visual == highlighted) {
-			node.visual.material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-		} else {
-			node.visual.material = node.player.material;
-		}
-	}
-
-	renderer.render(scene, camera);
+	stage.paint(renderer, camera);
 }
 
 function update() {
 	var time = Date.now();
-	//console.log(time);
-	var ticks_cap = 30;
-	while (time > next_tick) {
-		FleetEngine.tick();
-		next_tick += next_tick_delta;
-		if (--ticks_cap == 0) {
-			next_tick = time + next_tick_delta;
-			break;
-		}
-	}
-
-	if (ticks_cap < 30) {
-		if (highlighted) {
-			console.log(highlighted.game_node);
-		}
-	}
-
+	stage.update(time);
 	paint();
 }
 
@@ -233,32 +374,31 @@ function onWindowResize() {
 
 function onMouseDown(e) {
 	e.preventDefault();
-	mouse.x = e.clientX;
-	mouse.y = e.clientY;
 
-	object = pick();
+	var p = to_screen(e);
+	mouse.x = p.x;
+	mouse.y = p.y;
 
-	console.log("pick", object.game_node);
-
-	if (!object) {
-		selected = undefined;
-	}
-
-	if (selected && selected.game_node.player == player) {
-		FleetEngine.add_fleet(player, selected.game_node, object.game_node, 0.5);
-		selected = undefined;
-	} else {
-		selected = object;
-	}
-
+	source = stage.pick(p.x, p.y, camera);
 	return false;
+}
+
+function onMouseUp(e) {
+	e.preventDefault();
+	var p = to_screen(e);
+
+	object = pick(p.x, p.y);
+
+	if (object && source && source.player == player) {
+		stage._engine.sendFleet(player, source, object, source.population / 2);
+	}
+	source = null;
 }
 
 function onMouseMove(e) {
 	e.preventDefault();
-	mouse.x = e.clientX;
-	mouse.y = e.clientY;
-
-	highlighted = pick();
+	var p = to_screen(e);
+	var obj = stage.pick(p.x, p.y, camera);
+	highlighted = obj ? [obj] : null;
 	return false;
 }
